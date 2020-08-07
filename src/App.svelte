271eaa6895fb550370fc62474324a7e8efc984ec
Nicolas Le Thierry d'Ennequin <nlte@magic.fr>
2020-08-07 16:54:14 +0200

    Random color

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,17 @@
 <script>
+  import Card from "./Card.svelte";
+
+  let cards = new Array(20).fill().map(() => {
+    return Math.floor(Math.random() * 16777215).toString(16);
+  });
+
   // export let name;
 </script>
 
 <style>
   main {
     display: flex;
-    align-items: center; /* flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: flex-start;
-    align-content: flex-start;
- */
-
+    align-items: center;
     width: 100%;
     margin: 0 auto;
     max-width: 66.67vh;
@@ -23,41 +24,34 @@
     grid-template-columns: 1fr 1fr 1fr 1fr;
     width: 100%;
   }
-
-  .board > div {
-    background-color: #69f;
-    outline: solid 1px #40404a;
-    cursor: pointer;
-  }
-
-  .board > div:before {
-    display: inline-block;
-    content: "";
-    padding-bottom: 100%;
-  }
 </style>
 
 <main>
   <div class="board">
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
+    {#each cards as card}
+      <Card color={card} />
+    {/each}
+    <!--
+		<div style="background-color:#369;" />
+    <div style="background-color:#379;" />
+    <div style="background-color:#469;" />
+    <div style="background-color:#468;" />
+    <div style="background-color:#368;" />
+    <div style="background-color:#269;" />
+    <div style="background-color:#279;" />
+    <div style="background-color:#378;" />
+    <div style="background-color:#257;" />
+    <div style="background-color:#267;" />
+    <div style="background-color:#458;" />
+    <div style="background-color:#258;" />
+    <div style="background-color:#278;" />
+    <div style="background-color:#457;" />
+    <div style="background-color:#27a;" />
+    <div style="background-color:#36a;" />
+    <div style="background-color:#46a;" />
+    <div style="background-color:#37a;" />
+    <div style="background-color:#369;" />
+    <div style="background-color:#369;" />
+ -->
   </div>
 </main>
---
+++ src/Card.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let color;
+</script>
+
+<style>
+  div {
+    cursor: pointer;
+  }
+
+  div:before {
+    display: inline-block;
+    content: "";
+    padding-bottom: 100%;
+  }
+</style>
+
+<div style="background-color:#{color || '69f'}" />
