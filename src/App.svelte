3bd0133f5147293eb1666de817918febad038467
Nicolas Le Thierry d'Ennequin <nlte@magic.fr>
2020-08-07 12:25:44 +0200

    Initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,63 @@
 <script>
-	export let name;
+  // export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    display: flex;
+    align-items: center; /* flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    align-content: flex-start;
+ */
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    width: 100%;
+    margin: 0 auto;
+    max-width: 66.67vh;
+    height: 100vh;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .board {
+    display: grid;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    width: 100%;
+  }
+
+  .board > div {
+    background-color: #69f;
+    outline: solid 1px #40404a;
+    cursor: pointer;
+  }
+
+  .board > div:before {
+    display: inline-block;
+    content: "";
+    padding-bottom: 100%;
+  }
 </style>
+
+<main>
+  <div class="board">
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+</main>
